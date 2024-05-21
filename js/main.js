098c1ca1fdf60592d07c4b9fdaf3be4b60513742
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-05-21 17:34:25 -0300

    feat: adding responsibility

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const buttonFavorite = document.querySelectorAll('.button-favorite');
-
+const inputSearch = document.getElementById('search');
+const postsList = document.querySelectorAll('#blog-posts > article');
 buttonFavorite.forEach((button)=>{
   button.addEventListener('click', ()=>{
     button.classList.toggle('active')
-  })
+  });
+});
+
+inputSearch.addEventListener('input', (e)=>{
+  
 })

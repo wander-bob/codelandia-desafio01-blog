a27b33b7a72590ba318c9735842217b4bb90a307
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-06-14 17:52:40 -0300

    feat: adding filter posts by DOM

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const buttonFavorite = document.querySelectorAll('.button-favorite');
 const inputSearch = document.getElementById('search');
-const postsList = document.querySelectorAll('#blog-posts > article');
+const posts = document.querySelectorAll('#blog-posts > article');
 buttonFavorite.forEach((button)=>{
   button.addEventListener('click', ()=>{
     button.classList.toggle('active')
   });
 });
 
-inputSearch.addEventListener('input', (e)=>{
-  
-})
+inputSearch.addEventListener('input', filterPosts)
+function filterPosts(){
+  if(inputSearch.value != ''){
+    for(let post of posts){
+      let title = post.querySelector('h2');
+      title = title.textContent.toLocaleLowerCase();
+      let filterTitle = inputSearch.value.toLocaleLowerCase();
+      if(!title.includes(filterTitle)){
+        post.style.display = 'none';
+      }else{
+        post.style.display = 'block';
+      }
+    }
+  }
+}
